f60cc9c63673c27bac77ff241c91a5f5707e205a
MohamedDer <mohamed.derkaoui@mts-system.com>
2018-07-31 14:57:23 +0100

    Initial Commit

---
+++ AV Market/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AV Market
+//
+//  Created by MTS 4 on 7/31/18.
+//  Copyright © 2018 Mohamed Derkaoui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
