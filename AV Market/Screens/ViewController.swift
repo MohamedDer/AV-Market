997eeb35aff18585845b0e17196c1ced93b01ee8
MohamedDer <mohamed.derkaoui@mts-system.com>
2018-07-31 16:35:16 +0100

    WIP list fetched items

---
+++ AV Market/Screens/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  AV Market
+//
+//  Created by MTS 4 on 7/31/18.
+//  Copyright © 2018 Mohamed Derkaoui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var adsArray: [Ad]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let df = DataFetcher()
+        df.fetchAds { ads in
+            self.adsArray = ads
+            self.tableView.reloadData()
+        }
+
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.adsArray?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "itemCell") as! itemTableViewCell
+        cell.titleLabel.text = self.adsArray?[indexPath.row].title
+        if let price = self.adsArray?[indexPath.row].price {
+            cell.priceLabel.text =   " \(price) DH"
+        } else {
+            cell.priceLabel.text = "  -  "
+        }
+        return cell
+    }
+
+
+}
+
---
+++ AV Market/Views/itemTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  itemTableViewCell.swift
+//  AV Market
+//
+//  Created by MTS 4 on 7/31/18.
+//  Copyright © 2018 Mohamed Derkaoui. All rights reserved.
+//
+
+import UIKit
+
+class itemTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var adImage: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+}
